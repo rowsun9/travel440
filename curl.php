9df6a2b0bb6a4c836ec27c35e1cdb830a7f4055e
rowsun9 <60671941+rowsun9@users.noreply.github.com>
2020-04-26 21:13:14 -0500

    Add files via upload

--- curl.php
+++ curl.php
@@ -11,9 +11,26 @@ $search = $_GET [ 'searchText' ];
 <html>
 <head><title>Search Results</title>
 <link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="yourtravelcss.css">
 </head>
 <body class="curl">
+<div class="header">
+  <h1>Welcome, to Your Travel</h1>
+  <p>Travel <b>Your</b> way.</p>
+      </div>
 
+<div class="navbar">
+    <a href="homepage.html" class="active">Home</a>
+    <a href="review.html">Reviews/Ratings</a>
+    <a href="destinationlandingpage.html">Destinations</a>
+    <a href="suggestedforyou.html">Suggested for You</a>
+    <a href="myaccount.html">My Account</a>
+    <a href="registration.php" class="right">Sign up here!</a>
+    <a href="login.php" class="right">Login Here!</a>
+<div class="clearfix">
+<div class="column">
+  <div class="content column">
+ 
 <?php
 if( !$button )
         echo "you didn't submit a keyword";
@@ -21,15 +38,15 @@ if( !$button )
             if( strlen( $search ) == 0 )
                 echo "Search term non existent";
             else { 
-                echo "You searched for <b> $search </b> <br> ";
-                echo "Following are the results: <hr size='1' > </ br >";
+                echo "<b> $search </b> <br>";
+                echo "Here is information about the destination: <hr size='1' > </ br >";
             }
 
-                //curl
+                //curl for pictures from google images
                 $curl = curl_init();
 
-                $search=str_replace(' ','+',$search);
-                $url =("https://www.google.com/search?q=".$search."&tbm=isch");
+                $search1=str_replace(' ','+',$search);
+                $url =("https://www.google.com/search?q=".$search1."&tbm=isch");
 
                 curl_setopt($curl,CURLOPT_URL, $url);
                 curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
@@ -40,7 +57,37 @@ if( !$button )
                 $domResults->load($result);
                 $domResults = $domResults->find('img');
 
+                curl_close($curl);
+                
+
+                $ch = curl_init();
+                $search2=str_replace(' ','_',$search);
+                $url2 = 'https://en.wikipedia.org/api/rest_v1/page/summary/'.$search2.'';
+                //$ch = curl_init($url2);
+                curl_setopt($ch,CURLOPT_URL, $url2);
+                curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                $c = curl_exec($ch);
+
+                $json = json_decode($c);
+              
+
+
 ?>
+
+  </div>
+</div>
+</div>
+
+<div>
+
+<?php
+    echo $json->extract;
+?>
+
+</div>
+
+<br>
 
 <div class="list">
         <table>
@@ -53,14 +100,14 @@ if( !$button )
 
                     }else{
                         if($counter%4 == 3){
-                            echo '<th class="h">'.'<img src="'.$pic->src.'">' . '</th></tr>';
+                            echo '<th class="parallax">'.'<img src="'.$pic->src.'">' . '</th></tr>';
                         }elseif($counter%4 == 2){
-                            echo '<th class="h">'.'<img src="'.$pic->src.'">' . '</th>' ;
+                            echo '<th class="parallax">'.'<img src="'.$pic->src.'">' . '</th>' ;
                         }elseif($counter%4 == 1){
-                            echo '<th class="h">'.'<img src="'.$pic->src.'">' . '</th>' ;
+                            echo '<th class="parallax">'.'<img src="'.$pic->src.'">' . '</th>' ;
                         }
                         else{
-                            echo '<tr><th class="h">'.'<img src="'.$pic->src.'">' . '</th>';
+                            echo '<tr><th class="parallax">'.'<img src="'.$pic->src.'">' . '</th>';
                         }
                     }
                     
@@ -71,11 +118,11 @@ if( !$button )
                 //foreach($domResults->find('a[href^=/url?]')as $link){
                     //echo $link->plaintext . '<br>';
                 //}
-                curl_close($curl);
+                
 
         }      
 
-
+curl_close($ch);
 
  ?>
  
--- server.php
+++ server.php
@@ -8,11 +8,16 @@ $errors = array();
 $db = mysqli_connect('localhost', 'root', '', 'registration');
 
 if (isset($_POST['reg_user'])) {
-  // receive all input values from the form
-  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $firstname = mysqli_real_escape_string($db, $_POST['firstname']);
+  $lastname = mysqli_real_escape_string($db, $_POST['lastname']);
+  $dob = mysqli_real_escape_string($db, $_POST['dob']);
+  $phone = mysqli_real_escape_string($db, $_POST['phone']);
   $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+  $password2 = mysqli_real_escape_string($db, $_POST['password2']);
+  $bio = mysqli_real_escape_string($db, $_POST['bio']);
+  $destination = mysqli_real_escape_string($db, $_POST['destination']);
 
   if (empty($username)) { array_push($errors, "Username is required"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
@@ -25,7 +30,7 @@ if (isset($_POST['reg_user'])) {
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
-  if ($user) { // if user exists
+  if ($user) { 
     if ($user['username'] === $username) {
       array_push($errors, "Username already exists");
     }
@@ -36,17 +41,17 @@ if (isset($_POST['reg_user'])) {
   }
 
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+  	$password = md5($password_1);
 
-  	$query = "INSERT INTO users (username, email, password) 
-  			  VALUES('$username', '$email', '$password')";
+  	$query = "INSERT INTO users (firstname, lastname, dob, phone, email, username, password, bio, destination)
+  			  VALUES('$firstname', '$lastname', '$dob', '$phone', '$email', $username', '$password', '$bio', '$destination')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are now logged in";
   	header('location: index.php');
   }
 }
-
+// LOGIN USER
 if (isset($_POST['login_user'])) {
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
@@ -71,6 +76,4 @@ if (isset($_POST['login_user'])) {
         }
     }
   }
-  
   ?>
-
